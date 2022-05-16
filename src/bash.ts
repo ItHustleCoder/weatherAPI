c849851a2943ba63864b883907c93ce2028e1b5a
Andriy Bohdan <61162113+ItHustleCoder@users.noreply.github.com>
2022-05-16 12:10:08 +0200

    Add try & catch block to handle errors

--- src/bash.ts
+++ src/bash.ts
@@ -14,17 +14,20 @@ const factory = new Factory();
 
 //This function will be runned immediately
 (async () => {
-    const api = await axios.get(factory.weatherUrl(address));
-    const { coord, name, weather } = api.data;
-    const yelpAPI = await axios.get(factory.yelpUrl(name), options);
+    try {
+            const api = await axios.get(factory.weatherUrl(address));
+            const { coord, name, weather } = api.data;
+            const yelpAPI = await axios.get(factory.yelpUrl(name), options);
 
-    let store:any = [];
+            let store:any = [];
     
-    yelpAPI.data.businesses.map((value:any) => {
-        return store.push({ name:value.name, status: value.is_closed ? 'Closed' : 'Open', rating:value.rating, location: value.location.address1, number: value.phone ? value.phone : 'Not available', distance: factory.decimalConversion(value.distance) });
-    })
+            yelpAPI.data.businesses.map((value:any) => {
+                return store.push({ name:value.name, status: value.is_closed ? 'Closed' : 'Open', rating:value.rating, location: value.location.address1, number: value.phone ? value.phone : 'Not available', distance: factory.decimalConversion(value.distance) });
+            })
 
-    const result = [{ ...{ city: name, coordinates: {lat: coord.lat, lon: coord.lon }, weather: weather[0].main }, }, store]; 
-    console.log(result);
-     
+            const result = [{ ...{ city: name, coordinates: {lat: coord.lat, lon: coord.lon }, weather: weather[0].main }, }, store]; 
+            console.log(result);
+        }catch(err:any) {
+            console.log("Error", err);
+        }
 })()
